0ca4e3ac18c6e20a653f5afd2683ffb18f4b2bea
Isabella334 <isabellarecinos@gmail.com>
2025-06-09 21:08:21 -0600

    hook integrated

--- src/Components/Calculator/Calculator.tsx
+++ src/Components/Calculator/Calculator.tsx
@@ -4,7 +4,7 @@ import Display from "../Display"
 import styles from "./Calculator.module.css"
 
 const Calculator = () => {
-  const { display, handleNumberInput } = useCalculator()
+  const { display, handleNumberInput, resetCalc, chooseOperation, executeCalculation } = useCalculator()
 
   return (
     <div className={styles.calc}>
@@ -12,25 +12,25 @@ const Calculator = () => {
         <Display value={display}/>
       </div>
       <div className={styles.keyboard}>
-        <Button label="C" onClick={() => {}}/>
-        <Button label="+/-" onClick={() => {}}/>
-        <Button label="%" onClick={() => {}}/>
-        <Button label="÷" onClick={() => {}}/>
+        <Button label="C" onClick={resetCalc}/>
+        <Button label="+/-" onClick={()=>{chooseOperation("+/-")}}/>
+        <Button label="%" onClick={() => {chooseOperation("%")}}/>
+        <Button label="÷" onClick={() => {chooseOperation("/")}}/>
         <Button label="7" onClick={()=>{handleNumberInput("7")}}/>
         <Button label="8" onClick={()=>{handleNumberInput("8")}}/>
         <Button label="9" onClick={()=>{handleNumberInput("9")}}/>
-        <Button label="x" onClick={() => {}}/>
+        <Button label="x" onClick={() => {chooseOperation("x")}}/>
         <Button label="4" onClick={()=>{handleNumberInput("4")}}/>
         <Button label="5" onClick={()=>{handleNumberInput("5")}}/>
         <Button label="6" onClick={()=>{handleNumberInput("6")}}/>
-        <Button label="-" onClick={() => {}}/>
+        <Button label="-" onClick={() => {chooseOperation("-")}}/>
         <Button label="1" onClick={()=>{handleNumberInput("1")}}/>
         <Button label="2" onClick={()=>{handleNumberInput("2")}}/>
         <Button label="3" onClick={()=>{handleNumberInput("3")}}/>
-        <Button label="+" onClick={() => {}}/>
+        <Button label="+" onClick={() => {chooseOperation("+")}}/>
         <Button label="0" onClick={()=>{handleNumberInput("0")}}/>
-        <Button label="." onClick={() => {}}/>
-        <Button label="=" onClick={() => {}}/>
+        <Button label="." onClick={() => {handleNumberInput(".")}}/>
+        <Button label="=" onClick={executeCalculation}/>
       </div>
     </div>
   )
