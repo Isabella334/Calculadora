0b7a05d0d775091be61c4b48ad10ed3a383deaf3
Isabella334 <isabellarecinos@gmail.com>
2025-06-09 19:49:16 -0600

    calc schema

---
+++ src/Components/Calculator/Calculator.tsx
@@ -0,0 +1,33 @@
+import Button from "../Button"
+import Display from "../Display"
+
+const Calculator = () => {
+  return (
+    <div>
+      <Display />
+      <div>
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+    </div>
+  )
+}
+
+export default Calculator
